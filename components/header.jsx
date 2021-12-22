afa7e543c0c58f1b384a412cf40c29dfe28e35e6
avflo <bastian.avflo@gmail.com>
2021-12-21 22:26:43 -0300

    :sparkles: add first hero section

--- components/header.jsx
+++ components/header.jsx
@@ -6,9 +6,24 @@ import {
   Flex,
   Text,
   Button,
-  useDisclosure
+  useDisclosure,
+  useColorModeValue,
+  IconButton,
+  onToggle,
+  useBreakpointValue ,
+  Popover,
+  PopoverTrigger,
+  Link,
+  PopoverContent,
+  Collapse,
+  Icon
 } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { 
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon
+} from "@chakra-ui/icons";
 
 // Note: This code could be better,
 // so I'd recommend you to understand how I solved and you could write yours better :)
@@ -16,57 +31,243 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 
 // Update: Check these awesome headers from Choc UI 👇
 // https://choc-ui.tech/docs/elements/headers
-const Header = (props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const handleToggle = () => (isOpen ? onClose() : onOpen());
 
+const Header = (props) => {
+  /* const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleToggle = () => (isOpen ? onClose() : onOpen()); */
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding={6}
-      bg="teal.500"
-      color="white"
-      {...props}
-    >
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-          Chakra UI
-        </Heading>
+    <Box>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        padding={6}
+        bg="white.500"
+        color="black"
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        {...props}
+      >
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}>
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              }
+              variant={'ghost'}
+              aria-label={'Toggle Navigation'}
+            />
+          </Flex>
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+            <Text
+              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+              fontFamily={'heading'}
+              color={useColorModeValue('gray.800', 'white')}>
+              Logo
+            </Text>
+          </Flex>
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav /> 
+          </Flex>
       </Flex>
 
-      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
-        <HamburgerIcon />
-      </Box>
-
-      <Stack
-        direction={{ base: "column", md: "row" }}
-        display={{ base: isOpen ? "block" : "none", md: "flex" }}
-        width={{ base: "full", md: "auto" }}
-        alignItems="center"
-        flexGrow={1}
-        mt={{ base: 4, md: 0 }}
-      >
-        <Text>Docs</Text>
-        <Text>Examples</Text>
-        <Text>Blog</Text>
-      </Stack>
-
-      <Box
-        display={{ base: isOpen ? "block" : "none", md: "block" }}
-        mt={{ base: 4, md: 0 }}
-      >
-        <Button
-          variant="outline"
-          _hover={{ bg: "teal.700", borderColor: "teal.700" }}
-        >
-          Create account
-        </Button>
-      </Box>
-    </Flex>
+      <Collapse in={isOpen} animateOpacity>
+       <MobileNav />
+      </Collapse>
+    </Box>
   );
 };
+
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                w={3}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}>
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}>
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+
+const DesktopSubNav = ({ label, href, subLabel }) => {
+  return (
+    <Link
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('yellow.50', 'gray.900') }}>
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'yellow.400' }}
+            fontWeight={500}>
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}>
+          <Icon color={'yellow.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      display={{ md: 'none' }}>
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? '#'}
+        justify={'space-between'}
+        align={'center'}
+        _hover={{
+          textDecoration: 'none',
+        }}>
+        <Text
+          fontWeight={600}
+          color={useColorModeValue('gray.600', 'gray.200')}>
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={'all .25s ease-in-out'}
+            transform={isOpen ? 'rotate(180deg)' : ''}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          align={'start'}>
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+/* interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+} */
+
+const NAV_ITEMS = [
+  {
+    label: 'Areas de acción',
+    children: [
+      {
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#',
+      },
+      {
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Blog',
+    href: '#',
+  },
+  {
+    label: 'Corporación',
+    href: '#',
+  },
+  {
+    label: 'Contacto',
+    href: '#',
+  },
+];
+
 
 export default Header;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,96 @@
-import Layout from '../components/layout';
-import { getAllFilesFrontMatter } from '../lib/mdx';
-import NextLink  from 'next/link';
-import { Fragment } from 'react';
+import Layout from "../components/layout"
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Heading,
+  Button,
+  Flex,
+  SimpleGrid,
+  Image,
+  Avatar,
+} from "@chakra-ui/react"
+import { getAllFilesFrontMatter } from "../lib/mdx"
+import NextLink from "next/link"
+import { Fragment } from "react"
 
 export default function Home({ posts }) {
   return (
     <Layout>
       <Fragment>
-        <div >
+        <div>
           <main>
-            <h1 className="title">
+            <Box bg="white" w="100%" p={5} color="">
+              <Container maxW={"8xl"}>
+                <SimpleGrid columns={2} spacing={12}>
+                  <Stack
+                    textAlign={"left"}
+                    align={"left"}
+                    spacing={{ base: 8, md: 10 }}
+                    py={{ base: 20, md: 28 }}
+                  >
+                    <Heading
+                      fontWeight={600}
+                      fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+                      lineHeight={"110%"}
+                    >
+                      <Text color={"black.400"}>Corporación Educacional </Text>
+                      <Text as={"span"} color={"orange.400"}>
+                        Paulo Freire
+                      </Text>
+                    </Heading>
+                    <Text color={"gray.500"} maxW={"lg"}>
+                      Educar para los nuevos desafíos, liderazgo, desarrollo
+                      sostenible y para ser protagonistas
+                    </Text>
+                    <Stack spacing={6} direction={"row"}>
+                      <Button
+                        rounded={"full"}
+                        px={6}
+                        colorScheme={"orange"}
+                        bg={"orange.400"}
+                        _hover={{ bg: "orange.500" }}
+                      >
+                        Leer Más
+                      </Button>
+                      <Button rounded={"full"} px={6}>
+                        Contáctanos
+                      </Button>
+                    </Stack>
+                    <Flex w={"full"}></Flex>
+                  </Stack>
+
+                  <Stack
+                    textAlign={"center"}
+                    align={"center"}
+                    spacing={{ base: 8, md: 10 }}
+                    py={{ base: 20, md: 28 }}
+                  >
+                    <Box w={"full"} y={"full"} bg={"white"} overflow={"hidden"}>
+                      <Image
+                        objectFit="cover"
+                        src={"/images/background_blob.svg"}
+                        alt="bblob"
+                        pt={'3'}
+                      />
+                    </Box>
+                    <Box position="absolute"  justify={"center"}>
+                      <Image
+                        boxSize={'390'}
+                        objectFit="cover"
+                        src={"/images/kid.jpg"}
+                        alt={"Author"}
+                        borderRadius={'md'}
+                      />
+                    </Box>
+                    {/* <Image src={"/images/kid.jpg"} alt="student"/> */}
+                  </Stack>
+                </SimpleGrid>
+              </Container>
+            </Box>
+            <Box bg="#FFFCE3" w="100%" p={5} color="white"></Box>
+            {/*  <h1 className="title">
               BLOG
             </h1>
 
@@ -26,19 +107,18 @@ export default function Home({ posts }) {
                   </a>            
                 </NextLink>
               ))}
-            </div>
+            </div> */}
           </main>
         </div>
       </Fragment>
-
     </Layout>
   )
 }
 
-export async function getStaticProps(){
-  const posts = await getAllFilesFrontMatter("posts");
-  console.log(posts);
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter("posts")
+  console.log(posts)
   return {
-    props: {posts},
+    props: { posts },
   }
 }
